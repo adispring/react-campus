3de96d8f5e8d0f3882690712a32fc61f1ca171fa
wangzengdi <sunnyadi@163.com>
2023-05-26 11:33:25 +0800

    feat: 丰富 webpack 配置，支持 css; 学完 react 快速入门
    https://zh-hans.react.dev/learn

--- index.tsx
+++ index.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, React1!</h1>
+      <h1>Hello, React!</h1>
+      <MyButton />
+      <img className="avatar" alt="haha"/>
+      <ul>{listItems}</ul>
     </div>
   );
 };

49672dbf8267cb6f450519ead0f726d9b851f7c4
wangzengdi <sunnyadi@163.com>
2023-05-31 18:55:43 +0800

    chore: 组件大写

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import About from './components/About';
 import NotFound from './components/NotFound';
 import Game from './components/Game';
-import Mobx from './components/Mobx1';
+import Mobx from './components/Mobx';
 
 const App = () => {
   return (

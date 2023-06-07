da2e0ced0c70ce426710c4986e7126792db31efb
wangzengdi <sunnyadi@163.com>
2023-06-07 10:07:04 +0800

    feat: mobx useEffect to update state

--- src/components/MobxUseState.tsx
+++ src/components/MobxUseState.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
 import { Timer } from '../stores/Timer';
 
 const TimerView = observer(() => {
   const [timer] = useState(() => new Timer());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      timer.increaseTimer();
+    }, 1000);
+    console.log('setInterval', interval);
+    return () => {
+      console.log('clearInterval', interval);
+      clearInterval(interval);
+    };
+  }, [timer]);
   return <span>Seconds passed: {timer.secondsPassed}</span>;
 });
 

bf54939b7c0c0776bd11851e06413b6c9cad44cd
wangzengdi <sunnyadi@163.com>
2023-06-21 17:21:53 +0800

    feat: jsdoc 注释

--- src/components/TodoList/interfaces.ts
+++ src/components/TodoList/interfaces.ts
@@ -1,18 +1,29 @@
 /** 待办事项 */
 export interface Todo {
-  id: number; // 唯一标识
-  text: string; // 内容
-  completed: boolean; // 是否已完成
+  /** 唯一标识 */
+  id: number;
+
+  /** 内容 */
+  text: string;
+
+  /** 是否已完成 */
+  completed: boolean;
 }
 
 /** Input 组件的 Props */
 export interface InputProps {
-  onAdd: (value: string) => void; // 添加待办事项的回调函数
+  /** 添加待办事项 */
+  onAdd: (value: string) => void;
 }
 
 /** Item 组件的 Props */
 export interface ItemProps {
-  todo: Todo; // 待办事项数据
-  onDelete: (id: number) => void; // 删除待办事项的回调函数
-  onToggle: (id: number) => void; // 切换待办事项完成状态的回调函数
+  /** 待办事项数据 */
+  todo: Todo;
+
+  /** 删除待办事项 */
+  onDelete: (id: number) => void;
+
+  /** 切换待办事项完成状态 */
+  onToggle: (id: number) => void;
 }

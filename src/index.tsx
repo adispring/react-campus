1a72baf471c2b919924ca4707667c13f14a1e043
wangzengdi <sunnyadi@163.com>
2023-07-01 08:41:42 +0800

    feat: 简化文件夹

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './styles.css';
 
-import App from './App';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/todo-list" element={<TodoList />} />
+        <Route path="/" element={<TodoList />} />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('root')!).render(<App />);
